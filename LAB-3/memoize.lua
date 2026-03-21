886241d6b981f6e227e000273e9918e47556eba6
Developer <ghostmoonway@gmail.com>
2026-03-21 17:13:37 +0200

    L3 | Cache limit and options table

--- LAB-3/memoize.lua
+++ LAB-3/memoize.lua
@@ -6,13 +6,23 @@ local function make_key(args)
     end
     return table.concat(parts, "|")
 end
-function memoize.new(fn)
+local function get_size(t)
+    local n = 0
+    for _ in pairs(t) do n = n + 1 end
+    return n
+end
+function memoize.new(fn, options)
+    options = options or {}
+    local max_size = options.max_size
     local cache = {}
     return function(...)
         local args = {...}
         local key = make_key(args)
         if cache[key] ~= nil then
             return cache[key]
+        end
+        if max_size and get_size(cache) >= max_size then
+            return fn(...)
         end
         local result = fn(...)
         cache[key] = result
