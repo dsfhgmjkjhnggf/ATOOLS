b53ddb9c91cae27311b8072952716c77be2c6840
Developer <ghostmoonway@gmail.com>
2026-04-04 02:59:34 +0300

    L4 | Dequeue

--- LAB-4/bipq.lua
+++ LAB-4/bipq.lua
@@ -28,6 +28,28 @@ function bipq.new()
         end
         return result.item, result.priority, result.order
     end
+    function pq:dequeue(mode)
+        if #self._data == 0 then return nil end
+        local result_idx = 1
+        local result = self._data[1]
+        for idx, entry in ipairs(self._data) do
+            if mode == "highest" and entry.priority > result.priority then
+                result = entry
+                result_idx = idx
+            elseif mode == "lowest" and entry.priority < result.priority then
+                result = entry
+                result_idx = idx
+            elseif mode == "oldest" and entry.order < result.order then
+                result = entry
+                result_idx = idx
+            elseif mode == "newest" and entry.order > result.order then
+                result = entry
+                result_idx = idx
+            end
+        end
+        table.remove(self._data, result_idx)
+        return result.item, result.priority, result.order
+    end
     return pq
 end
 return bipq
