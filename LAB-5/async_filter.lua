c0dafc409bdbe8dd90a88dc79c334cda070ad5fe
Developer <ghostmoonway@gmail.com>
2026-04-18 19:42:14 +0300

    L5 | Promise

--- LAB-5/async_filter.lua
+++ LAB-5/async_filter.lua
@@ -31,4 +31,37 @@ function async_filter.filterCallback(arr, predicate, callback)
         step()
     end
 end
+local function newPromise(fn)
+    local promise = {}
+    local _resolve, _reject
+    local _then_cb, _catch_cb
+    function promise:andThen(cb)
+        _then_cb = cb
+        return self
+    end
+    function promise:catch(cb)
+        _catch_cb = cb
+        return self
+    end
+    _resolve = function(value)
+        if _then_cb then _then_cb(value) end
+    end
+    _reject = function(err)
+        if _catch_cb then _catch_cb(err) end
+    end
+    fn(_resolve, _reject)
+    return promise
+end
+
+function async_filter.filterPromise(arr, predicate)
+    return newPromise(function(resolve, reject)
+        async_filter.filterCallback(arr, predicate, function(err, result)
+            if err then
+                reject(err)
+            else
+                resolve(result)
+            end
+        end)
+    end)
+end
 return async_filter
