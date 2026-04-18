f5e6e730b50ea13922af8f813e86d831d0f2d267
Developer <ghostmoonway@gmail.com>
2026-04-18 19:10:35 +0300

    L5 | Filter callback

--- LAB-5/async_filter.lua
+++ LAB-5/async_filter.lua
@@ -8,4 +8,27 @@ function async_filter.filter(arr, predicate)
     end
     return result
 end
+function async_filter.filterCallback(arr, predicate, callback)
+    local co = coroutine.create(function()
+        local result = {}
+        for i, v in ipairs(arr) do
+            local ok = predicate(v, i)
+            coroutine.yield()
+            if ok then
+                table.insert(result, v)
+            end
+        end
+        callback(nil, result)
+    end)
+    local function step()
+        local ok, err = coroutine.resume(co)
+        if not ok then
+            callback(err, nil)
+        end
+    end
+    step()
+    while coroutine.status(co) == "suspended" do
+        step()
+    end
+end
 return async_filter
